000116f06acee12f5c4ebd9a8bc46a8149fe9c17
fmadieva <fariza.madieva@gmail.com>
2020-04-22 20:21:27 -0500

    new project

--- src/test/java/com/weborders/utilities/ConfigurationReader.java
+++ src/test/java/com/weborders/utilities/ConfigurationReader.java
@@ -1,4 +1,4 @@
-package com.weborders.utilities.utilities;
+package com.weborders.utilities;
 
 import java.io.FileInputStream;
 import java.util.Properties;
--- src/test/java/com/weborders/utilities/DateTimeUtilities.java
+++ src/test/java/com/weborders/utilities/DateTimeUtilities.java
@@ -1,4 +1,4 @@
-package com.weborders.utilities.utilities;
+package com.weborders.utilities;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
